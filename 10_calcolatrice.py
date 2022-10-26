ec4ba564b9b9a80d277602cdb973888e54509721
chridemo <115648805+chridemo@users.noreply.github.com>
2022-10-26 15:01:07 +0200

    [ADD] 10° Programma

---
+++ 10_calcolatrice.py
@@ -0,0 +1,21 @@
+numero1 = int(input("inserisci un numero: "))
+numero2 = int(input("inserisci un altro numero: "))
+operazione = int(input("scegli l'operazione: 1 per +, 2 per -, 3 per /, 4 per * "))
+if(operazione == 1):
+    print(numero1 + numero2)
+if(operazione == 2):
+    print(numero1 - numero2)
+if(operazione == 3):
+    print(numero1 / numero2)
+else:
+    if(numero1 == 0):
+        print("Non puoi dividere per 0! ") 
+    if(numero2 == 0):
+        print("Non puoi dividere per 0! ")
+if(operazione == 4):
+    print(numero1 * numero2)
+
+if(operazione >= 5):
+    operzaione = 1
+
+input("pess any key to exit")
