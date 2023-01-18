de6f9ae086354477e80684d314a31b29d96f328e
Christian De Molli <115648805+chridemo@users.noreply.github.com>
2023-01-18 23:12:20 +0100

    Add files via upload

---
+++ 101_stringa_libro.py
@@ -0,0 +1,8 @@
+i: int = 0
+text: str = "Nel mezzo del cammin di nostra vita mi trovai"
+count_text = len(text)
+while i < count_text:
+    n: str = text[i]    
+    print(str(n))
+    i = i + 1
+    
